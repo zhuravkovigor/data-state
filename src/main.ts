c2aaf176f0ef8bc13c0ca438577e947be2e38899
Igor <zhuravkovigor@icloud.com>
2023-10-08 20:52:26 +0500

    done with sum states values

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,37 @@ const initStates = (states: any) => {
     const elements = document.querySelectorAll('[data-state]');
 
     elements.forEach((element: any) => {
-        const stateName = element.getAttribute('data-state');
-        element.innerHTML = states[stateName];
+        const text = element.getAttribute('data-state');
+        // if text has + inside brackets {stateName + stateName + ...states} then sum the states together
+
+        // if text has + - * / inside brackets {stateName} then do math with the states
+        if (text.match(/{(\w+)(\s*[\+\-\*\/]\s*\w+)+}/g)) {
+            const matches = text.match(/{(\w+)(\s*[\+\-\*\/]\s*\w+)+}/g);
+
+            matches.forEach((match: any) => {
+                // check if math has a text then just join values
+                if (match.match(/(\w+)/g).length > 1) {
+                    const result = match.match(/(\w+)/g).reduce((acc: any, stateName: any) => acc + states[stateName], "");
+
+                    text.replace(match, "");
+
+                    element.innerHTML = text.replace(/{(\w+)(\s*[\+\-\*\/]\s*\w+)+}/g, result);
+
+                    return;
+                }
+
+                const result = eval(match.replace(/(\w+)/g, (match: any, stateName: any) => states[stateName]))
+
+                text.replace(match, "");
+
+                element.innerHTML = text.replace(/{(\w+)(\s*[\+\-\*\/]\s*\w+)+}/g, result);
+            });
+
+            return;
+        }
+
+
+        element.innerHTML = text.replace(/{(\w+)}/g, (match, state) => states[state]);
     });
 
     return new Proxy(states, {
@@ -20,11 +49,10 @@ const initStates = (states: any) => {
             target[prop] = value;
 
             elements.forEach((element: any) => {
-                const stateName = element.getAttribute('data-state');
-                if (stateName === prop) {
-                    element.innerHTML = value;
-                }
+                const text = element.getAttribute('data-state');
+                element.innerHTML = text.replace(/{(\w+)}/g, (match, state) => states[state]);
             });
+
             return true;
         }
     });
